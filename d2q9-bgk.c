6e0ab0c0f768270a2d0a55f10cf69d670f6fea8a
Jerry Kress <jerrykress@icloud.com>
2020-04-20 20:00:44 +0100

    Removed timestep Write Data
    Reynolds number:                9.754217147827E+00
    Elapsed time:                   8.852078 (s)
    Elapsed user CPU time:          8.316144 (s)
    Elapsed system CPU time:        1.657603 (s)

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -232,9 +232,9 @@ int timestep(const t_param params, t_speed *cells, t_speed *tmp_cells, int *obst
 {
   cl_int err;
 
-  // Write cells to device
-  err = clEnqueueWriteBuffer(ocl.queue, ocl.cells, CL_TRUE, 0, sizeof(t_speed) * params.nx * params.ny, cells, 0, NULL, NULL);
-  checkError(err, "writing cells data", __LINE__);
+  // // Write cells to device
+  // err = clEnqueueWriteBuffer(ocl.queue, ocl.cells, CL_TRUE, 0, sizeof(t_speed) * params.nx * params.ny, cells, 0, NULL, NULL);
+  // checkError(err, "writing cells data", __LINE__);
 
   accelerate_flow(params, cells, obstacles, ocl);
   propagate(params, cells, tmp_cells, ocl);
